07ef567782603f0de146f92e262b4902229d6341
ilker Cantekin <454545ilker@gmail.com>
2022-11-15 00:01:21 +0300

    metodlar

--- src/Methodlarim/palindromeRecursiveMethods.java
+++ src/Methodlarim/palindromeRecursiveMethods.java
@@ -1,7 +1,22 @@
 package Methodlarim;
 
-public class resMethods {
+public class palindromeRecursiveMethods {
     public static void main(String[] args) {
+// palindrome= tersiyel düzü aynı olan sayı veya kelime
+       String input="ey edip adanada pide ye";
+       boolean sonuc=palindromeKontrolEt(input);
+        System.out.println(sonuc);
+    }
+
+    private static boolean palindromeKontrolEt(String input) {
+        if (input.length()==0 || input.length()==1){
+            return true;
+        }else if (input.substring(0,1).equalsIgnoreCase(input.substring(input.length()-1))){
+            return palindromeKontrolEt(input.substring(1,(input.length()-1)));
+
+        }else {
+            return false;
+        }
 
     }
 }
